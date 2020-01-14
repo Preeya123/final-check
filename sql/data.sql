2f78de3606448ce516474504d718e33b0602b3d5
B Mujagond, Priyanka (Cognizant) <Priyanka.BMujagond@cognizant.com>
2020-01-14 18:03:30 +0530

    SQL

--- sql/data.sql
+++ sql/data.sql
@@ -1,5 +1,8 @@
 -- Include table data insertion, updation, deletion and select scripts
 
+-- -------------------------------------------------------------------
+-- Adding Menu Items is MenuItem Table
+-- --------------------------------------------------------------------
 insert into movie
 values
 (1, 'Avatar', 2787965087, 'Yes','2017-03-15','Science Fiction','Yes'),
@@ -8,65 +11,90 @@ values
 (4, 'Jurassic World', 1671713208, 'No','2017-07-02','Science Fiction','Yes'),
 (5, 'Avengers:End Game', 2750760348, 'Yes','2022-11-02','SuperHero','Yes');
 
-select * from movie;
+select
+mo_id as "ID",
+mo_title as "Title",
+mo_box_Office as "Box Office",
+mo_active as "Active",
+mo_date_Of_launch as "Date Of Launch",
+mo_genre as "Genre",
+mo_has_Teaser as "Has Teaser"
+from movie;
 -- -------------------------------------------------------------------
--- Updating movie in movie_list Table
+-- Edit movie Item in Movie Crusier table 
 -- -------------------------------------------------------------------
-
-select * from movie;
 update movie 
-set mo_title='Stree',mo_boxOffice='57375967963',mo_active='Yes',mo_date_of_launch='2017-07-07',mo_genre='Haunted',mo_has_Teaser='Yes'
+set mo_title='Stree',mo_boxOffice='57375967963',mo_active='Yes',mo_date_Of_launch='2017-07-07',mo_genre='Haunted',mo_has_Teaser='Yes'
 where mo_id='1';
 -- -------------------------------------------------------------------
--- Adding movie into movie_list Table
+-- setting User Namr and Id
 -- -------------------------------------------------------------------
 insert into user(us_id,us_name)
 values
 (1,'The None'),(2,'GoodNews');
-select * from user;
--- -------------------------------------------------------------------
--- Updating into Menu Items is MenuItem Table
--- -------------------------------------------------------------------
-update user
-set us_name='Pink'
-where us_id=1;
+select 
+us_id as "User ID",
+us_name as "User Name"
+from user;
 
 -- -------------------------------------------------------------------
--- selecting menu item table
+-- Displaying Customer Movie List in MovieItem table
 -- -------------------------------------------------------------------
-
-select mo_id,mo_title,mo_boxOffice,mo_genre,mo_has_Teaser
+select 
+mo_title as "Title",
+mo_boxOffice as "Box Office",
+mo_genre as "Genre",
+mo_has_Teaser as "Has Teaser"
 from movie
-where mo_active='Yes'and mo_date_of_launch<(select(current_date()));
+where mo_active='Yes'and mo_date_of_launch<curdate();
 -- -------------------------------------------------------------------
--- Add favourite into favourite table
+-- Add to Favourite table
 -- -------------------------------------------------------------------
 insert into favourite(fv_id,fv_us_id,fv_pr_id)
 values
 (123,1,2),(124,1,1);
-select * from favourite;
+select 
+fv_id as "Favourite ID",
+fv_us_id as "Favourite User ID",
+fv_pr_id as "Favourite Primary ID"
+from favourite;
 
 -- -------------------------------------------------------------------
--- Editing List into movie favourite table
+-- Show Favourites Movie Item table
 -- -------------------------------------------------------------------
-select mo_title,mo_boxOffice,mo_active,mo_date_of_launch,mo_has_Teaser
+select 
+mo_title as "Title",
+mo_boxOffice as "Box Office",
+mo_genre as "Genre"
 from movie
 inner join favourite as favourite
 on fv_pr_id=mo_id
 where fv_us_id=1;
-
-select count(mo_boxOffice) as 'no of favourite'
+-- -------------------------------------------------------------------
+-- Total Price of Favourite Movie Item
+-- -------------------------------------------------------------------
+select count(mo_boxOffice) as "no of favourite"
 from movie
 inner join favourite as favourite
 on fv_pr_id=mo_id
-where fv_us_id=1;
--- -------------------------------------------------------------------
--- Remove items from the cart table
--- -------------------------------------------------------------------
-delete from favourite
-where fv_us_id=2 and fv_pr_id=4;
--- -------------------------------------------------------------------
--- Select list from favourite table
--- -------------------------------------------------------------------
-select*from favourite;
+where fv_us_id=1;  
+ -- -------------------------------------------------------
+-- -------remove movie from favorite------------------
+-- -------------------------------------------------------
+ delete from favorite
+ where  fv_us_id=1 
+ and fv_pr_id=3;
+  -- -------------------------------------------------------
+-- -------after deleting movie from the favorite ------------------
+-- -------------------------------------------------------
+ 
+  select
+fv_id as "Favorite ID",
+fv_us_id as "Favorite User ID",
+fv_pr_id as "Favorite Primary ID"
+from favorite 
+ 
+
+
+
 
